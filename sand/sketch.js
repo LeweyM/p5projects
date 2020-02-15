05ebe02d5a52d48a0bbab04c7f13874e5231fb33
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-02-15 14:34:41 +0100

    added walls

--- sand/sketch.js
+++ sand/sketch.js
@@ -1,14 +1,44 @@
+/// <reference path="./p5.d/p5.global-mode.d.ts" />
 
-let pixels = []
-let nextPixels = []
+const EMPTY = 0;
+const SAND = 1;
+const WALL = 2;
+const cellTypeMap = {
+	"EMPTY" : 0,
+	"SAND" : 1,
+	"WALL" : 2
+}
 const res = 50
 const scale = 400 / res
-let directionSwitcher = true;
+
+const pixels = []
+let removeMode = false
+let activeCellType = "SAND"
+
 
 function setup() {
 	const cnv = createCanvas(400, 400)
 
-	cnv.mousePressed(switchCell)
+	cnv.mousePressed(() => {
+		const row = floor(mouseY / scale)
+		const col = floor(mouseX / scale)
+		if (pixels[col][row].z != EMPTY) {
+			removeMode = true
+		}
+	}) 
+	cnv.mouseReleased(() => {
+		console.log("released")
+		removeMode = false
+	})
+
+	const sandButton = createButton("sand")
+	sandButton.position(10, 10)
+	sandButton.mousePressed(() => {activeCellType = "SAND"})
+
+
+	const wallButton = createButton("wall")
+	wallButton.position(50, 10)
+	wallButton.mousePressed(() => {activeCellType = "WALL"})
 
 	for (let i = 0; i < res; i++) {
 		const row = []
@@ -34,97 +64,9 @@ function processSand(x, y) {
 	}
 }
 
-function processWaterCopy() {
-	const nextPixels = []
-	for (let i = 0; i < res; i++) {
-		const row = []
-		for (let j = 0; j < res; j++) {
-			row.push(createVector(pixels[i][j].x, pixels[i][j].y, pixels[i][j].z))
-		}
-		nextPixels.push(row)
-	}
-
-	for (var x = res - 1; x >= 0; x--) {
-		for (var y = res - 1; y >= 0; y--) {
-
-			if (pixels[x][y].z == 1) {
-				if (pixelEmpty(x, y + 1)) {
-					moveCellDownCopy(x, y, nextPixels)
-				} else if (pixelEmpty(x - 1, y + 1)) {
-					moveCellDownLeft(x, y, pixels)
-				} else if (pixelEmpty(x + 1, y + 1)) {
-					moveCellDownRight(x, y, pixels)
-				} else if (pixelEmpty(x + 1, y)) {
-					moveCellLeft(x, y, pixels)
-				} else if (pixelEmpty(x - 1, y)) {
-					moveCellRight(x, y, pixels)
-				} else {
-					console.log("cell still")
-				}
-			}
-
-		}
-	}
-
-	return nextPixels
-}
-
-function processWater(x, y) {
-	if (pixels[x][y].z == 1) {
-		if (pixelEmpty(x, y + 1)) {
-			moveCellDown(x, y)
-		} else {
-			directionSwitcher ? spreadLeftFirst(x, y) : spreadRightFirst(x, y)
-			directionSwitcher = !directionSwitcher
-		}
-	}
-}
-
-function spreadLeftFirst(x, y, pixels) {
-	if (pixelEmpty(x - 1, y + 1)) {
-		moveCellDownLeft(x, y, pixels)
-	} else if (pixelEmpty(x + 1, y + 1)) {
-		moveCellDownRight(x, y, pixels)
-	} else if (pixelEmpty(x + 1, y)) {
-		moveCellLeft(x, y, pixels)
-	} else if (pixelEmpty(x - 1, y)) {
-		moveCellRight(x, y, pixels)
-	} else {
-		console.log("cell lost")
-	}
-}
-
-function spreadRightFirst(x, y) {
-	if (pixelEmpty(x + 1, y + 1)) {
-		moveCellDownRight(x, y)
-	} else if (pixelEmpty(x - 1, y + 1)) {
-		moveCellDownLeft(x, y)
-	} else if (pixelEmpty(x - 1, y)) {
-		moveCellRight(x, y)
-	} else if (pixelEmpty(x + 1, y)) {
-		moveCellLeft(x, y)
-	}
-}
-
-function moveCellLeft(x, y, pixels) {
-	pixels[x][y].z = 0
-	pixels[x+1][y].z = 1
-}
-
-function moveCellRight(x, y, pixels) {
-	pixels[x][y].z = 0
-	pixels[x-1][y].z = 1
-}
-
 function moveCellDown(x, y) {
 	pixels[x][y].z = 0
 	pixels[x][y + 1].z = 1
-}
-
-
-function moveCellDownCopy(x, y, copy) {
-	copy[x][y].z = 0
-	copy[x][y + 1].z = 1
 }
 
 function moveCellDownLeft(x, y, pixels) {
@@ -149,13 +91,13 @@ function notLastRow(y) {
 	return y < pixels.length - 1 
 }
 
-function switchCell() {
+function switchCell(newCellType) {
 	const row = floor(mouseY / scale)
 	const col = floor(mouseX / scale)
-	if (pixels[col][row].z == 1) {
-		pixels[col][row].z = 0
+	if (removeMode) {
+		pixels[col][row].z = EMPTY
 	} else {
-		pixels[col][row].z = 1
+		pixels[col][row].z = newCellType
 	}
 }
 
@@ -163,7 +105,7 @@ function draw() {
 	background(220)
 
 	if (mouseIsPressed) {
-		switchCell()
+		switchCell(cellTypeMap[activeCellType])
 	}
 
 	for (var i = res - 1; i >= 0; i--) {
@@ -172,15 +114,23 @@ function draw() {
 		}
 	}
 
-	// nextPixels = processWaterCopy()
-
 	for (row of pixels) {
 		for (p of row) {
- 			fill(p.z * 255, 229, 170)
-			noStroke()
-			rect(p.x, p.y, 10, 10)
+			if (p.z == SAND) {
+				fill(255, 229, 170)
+				noStroke()
+				rect(p.x, p.y, 10, 10)
+			}
+			if (p.z == WALL) {
+				fill(0, 0, 0)
+				noStroke()
+				rect(p.x, p.y, 10, 10)
+			}
+			if (p.z == EMPTY) {
+				fill(0, 229, 170)
+				noStroke()
+				rect(p.x, p.y, 10, 10)
+			}
 		}
 	}
-
-	// pixels = nextPixels;
 }
