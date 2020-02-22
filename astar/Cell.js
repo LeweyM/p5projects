9d1644b2779d59387b0f5cd616c8bb3b56aa49e6
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-02-22 11:38:21 +0100

    [astar] extract cells and grid to classes

---
+++ astar/Cell.js
@@ -0,0 +1,15 @@
+/// <reference path="./p5.global-mode.d.ts" />
+
+class Cell {
+    positionVector;
+    isStart;
+    isFinish;
+
+    constructor(positionVector) {
+        this.positionVector = positionVector
+        this.isStart = false;
+        this.isFinite = false;
+    }
+
+
+}
